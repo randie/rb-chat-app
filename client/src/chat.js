ba7e7c464f959dcaf4083b5ca47bf0f672b8303d
Randie B <randie.bemis@gmail.com>
2020-01-24 16:56:20 -0800

    simplify addMessage handling

--- client/src/chat.js
+++ client/src/chat.js
@@ -6,16 +6,12 @@ import MessageList from './message-list';
 function Chat({ user }) {
   const { messages, addMessage } = useChatMessages();
 
-  const handleSend = async text => {
-    await addMessage(text);
-  };
-
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Chatting as {user}</h1>
         <MessageList user={user} messages={messages} />
-        <MessageInput onSend={handleSend} />
+        <MessageInput onSend={addMessage} />
       </div>
     </section>
   );
